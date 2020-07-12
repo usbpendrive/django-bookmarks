b8db3d73d9817b30a8debaa2d414e3079811af9a
usbpendrive <ferry.fernando@gmail.com>
2020-07-12 13:30:50 +0700

    Fix wrong indentation

--- account/views.py
+++ account/views.py
@@ -20,6 +20,6 @@ def user_login(request):
                     return HttpResponse('Disabled account!')
             else:
                 return HttpResponse('Invalid login!')
-        else:
-            form = LoginForm()
-        return render(request, 'account/login.html', {'form': form})
+    else:
+        form = LoginForm()
+    return render(request, 'account/login.html', {'form': form})
